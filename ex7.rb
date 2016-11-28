4315b8385a7ff913f75d968aca4c04a532edd34f
frankie <franklin.shaw@gmail.com>
2016-11-28 15:51:46 +0000

    ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,38 @@
+#puts "Mary had a little lamb." to the console
+puts "Mary had a little lamb."
+#puts "Its fleece was white as snow" to the console
+puts "Its fleece was white as #{'snow'}."
+#puts "And everywhere that Mary went." to the console
+puts "And everywhere that Mary went."
+#puts ten '.' to the console
+puts "." * 10
+
+#creates and sets variable 'end1' to string 'C'
+end1 = "C"
+#creates and sets variable 'end2' to string 'h'
+end2 = "h"
+#creates and sets variable 'end3' to string 'e'
+end3 = "e"
+#creates and sets variable 'end4' to string 'e'
+end4 = "e"
+#creates and sets variable 'end5' to string 's'
+end5 = "s"
+#creates and sets variable 'end6' to string 'e'
+end6 = "e"
+#creates and sets variable 'end7' to string 'B'
+end7 = "B"
+#creates and sets variable 'end8' to string 'u'
+end8 = "u"
+#creates and sets variable 'end9' to string 'r'
+end9 = "r"
+#creates and sets variable 'end10' to string 'g'
+end10 = "g"
+#creates and sets variable 'end11' to string 'e'
+end11 = "e"
+#creates and sets variable 'end12' to string 'r'
+end12 = "r"
+
+#prints end1 to end6 wihtout a line break after
+print end1 + end2 + end3 + end4 + end5 + end6
+#puts end7 to end12 with a line break after
+puts end7 + end8 + end9 + end10 + end11 + end12
