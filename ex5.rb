1d6108b8a9bec49243cc9e9d378e898e0314225e
frankie <franklin.shaw@gmail.com>
2016-11-28 14:44:04 +0000

    ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,19 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in2009
+height = 74 # inches
+height1 = height * 2.54 #conversion to cm
+weight = 180 # lbs
+weight1 = weight * 0.453592 #conversion to Kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height} and #{weight} I get #{age + height + weight}."
