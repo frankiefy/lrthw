24b739dcd44b3a11fee99d2ae11f30a8ad27df55
frankie <franklin.shaw@gmail.com>
2016-11-28 13:40:19 +0000

    ex3.rb and calc

---
+++ ex3.rb
@@ -0,0 +1,28 @@
+#puts "I will now count my chickens:" to console
+puts "I will now count my chickens:"
+#puts "Hens #{reuslt of the math}" to the console
+puts "Hens #{25.0 + 30.0 / 6.0}"
+#puts "Roosters #{reuslt of the math}" to the console
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+#puts "Now I will count the eggs:" to console
+puts "Now I will count the eggs:"
+#does the math and just puts the answer to console
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#puts "Is it true that 3 + 2 < 5 - 7?" to console
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+#does the math and puts answer to console
+puts 3.0 + 2.0 < 5.0 - 7.0
+#puts "What is 3 + 2?" then the answer "5"
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+#puts "What is 5 - 7?" then the answer "-2"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+#puts "Oh, that's why it's false." to thge console
+puts "Oh, that's why it's false."
+#puts "How about some more." to the console
+puts "How about some more."
+#puts "Is it greater?" and "true" (the boolean answer to the > question)
+puts "Is it greater? #{5.0 > -2.0}"
+#puts "Is it greater or equal?" and "true"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+#puts "Is it less or equal?" and "false"
+puts "Is it less or equal? #{5.0 <= -2.0}"
