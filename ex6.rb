0497435004df345972e1ec4dd4a8a2f805b1a115
frankie <franklin.shaw@gmail.com>
2016-11-28 15:36:57 +0000

    ex6.rb

---
+++ ex6.rb
@@ -0,0 +1,39 @@
+#creating and setting variable 'types_of_people' to 10
+types_of_people = 10
+#creating and setting variable 'x' to a string containing a string interpolation
+x = "There are #{types_of_people} types of people."
+#creating and setting variable 'binary' to string 'binary'
+binary = "binary"
+#creating and setting variable 'do_not' to string 'don't'
+do_not = "don't"
+#creating and setting variable 'y' to a string containing 2 string interpolations
+y = "Those who know #{binary} and those who #{do_not}."
+
+#put varibale x to console
+puts x
+#put varibale y to console
+puts y
+
+#put "I said:" and the vaiable 'x' to console
+puts "I said: #{x}."
+#put "I also said" and the vaiable 'y' to console
+puts "I also said '#{y}'."
+
+#screating and etting vaiable hilarious to boolean 'false'
+hilarious = false
+#creating and setting joke_evaluation to a string containing string interpolation of the boolean variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#put varaible 'joke_evaluation' to console
+puts joke_evaluation
+
+#creating and setting variable 'w' to a string
+w = "This is the left side of..."
+#creating and setting variable 'e' to a string
+e = "a string with a right side."
+
+#put varibale w then varibale e on the same line with no space between
+puts w + e
+#the + (add) means both are put to the console in that order; together.
+#they dont work with '
+#unsure why, mentioned in previous exercises instructions
