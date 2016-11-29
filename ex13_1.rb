f0c2291363926b2a526cde62c1743e17f3c56e9e
frankie <franklin.shaw@gmail.com>
2016-11-29 15:49:24 +0000

    ex13.rb and _1 and _2

---
+++ ex13_1.rb
@@ -0,0 +1,4 @@
+first, second = ARGV
+
+puts "using the 1st variable #{first}"
+puts "using the 2nd variable #{second}"
---
+++ ex13_2.rb
@@ -0,0 +1,6 @@
+first, second, third, fourth = ARGV
+
+puts "using the 1st variable #{first}"
+puts "using the 2nd variable #{second}"
+puts "using the 3rd variable #{third}"
+puts "using the 4th variable #{fourth}"
