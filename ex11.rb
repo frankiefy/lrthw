ebb700e9b25d380f7af60cf5f5b38295336651c7
frankie <franklin.shaw@gmail.com>
2016-11-29 15:22:31 +0000

    edited ex11.rb

--- ex11.rb
+++ ex11.rb
@@ -5,3 +5,7 @@ height = gets.chomp
 print "How much do you weigh? "
 weight = gets.chomp
 puts "So, your'e #{age} old, #{height} tall and #{weight} heavy."
+
+
+print "tell me something"
+something = gets.chomp
